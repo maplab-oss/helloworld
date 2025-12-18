5c7f01c40bfcbd7417f9af407241e6bd85087c1c
Felix Sebastian <hi@felixsebastian.dev>
2025-12-18 12:21:13 +1100

    wip

--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -1,1 +1,5 @@
-export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+const backendHost = import.meta.env.VITE_BACKEND_HOST;
+
+export const apiBaseUrl =
+  baseUrl || (backendHost && `https://${backendHost}.onrender.com`) || "";
